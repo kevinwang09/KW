b9153174b6f45e92972c2fb976dac55b7a8fa07e
Kevin Wang <kevinwang@Kevins-Air.lan>
2017-11-07 21:28:59 +1100

    changed NAMESPACE

--- R/cleanDigits.R
+++ R/cleanDigits.R
@@ -3,15 +3,13 @@
 #' @param df A data frame with numbers and categorical variables
 #' @param sig Number of significant figures
 #' @return A data frame with less digits
-#' @examples
-#' cleanDigits(mtcars, 1)
 #'
 #'
 #'
 #'
 cleanDigits <- function(df, sig = 2){
   res = dplyr::mutate_if(.tbl = df,
-                  is.double,
-                  dplyr::funs(signif(., sig)))
+                         is.double,
+                         dplyr::funs(signif), digits = sig)
   return(res)
 }

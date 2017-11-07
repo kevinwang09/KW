46c96a8e20000192a4f6b874c4a2f0de5d46a706
Kevin Wang <kevinwang@vlan-2669-10-17-31-24.staff.wireless.sydney.edu.au>
2017-11-07 18:29:23 +1100

    cleanDigits

---
+++ R/cleanDigits.R
@@ -0,0 +1,17 @@
+#' Add together two numbers
+#'
+#' @param df A data frame with numbers and categorical variables
+#' @param sig Number of significant figures
+#' @return A data frame with less digits
+#' @examples
+#' cleanDigits(mtcars, 1)
+#'
+#'
+#'
+#'
+cleanDigits <- function(df, sig = 2){
+  res = dplyr::mutate_if(.tbl = df,
+                  is.double,
+                  dplyr::funs(signif(., sig)))
+  return(res)
+}

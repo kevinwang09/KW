b931d6c979e40ee61b58a1b1c81ec9a4e2d6ab3d
Kevin Wang <kevinwang@Kevins-Air.lan>
2017-11-11 13:05:56 +1100

    scale_colour_scotch

---
+++ R/scale_colour_scotch.R
@@ -0,0 +1,20 @@
+#' @title Scotch colours.
+#' @param n The number of colours
+#' @return ggproto object
+#' @examples
+#' df = data.frame(
+#' x = rnorm(100),
+#' y = rnorm(100),
+#' colour = factor(1:10))
+#' p = ggplot(df, aes(x = x, y = y, colour = colour)) +
+#' geom_point()
+#' p + scale_colour_scotch()
+#'
+scale_colour_scotch = function (n = n, ...)
+{
+  discrete_scale(aesthetics = "colour",
+                 scale_name = "scotch",
+                 palette = SmokyScotch::scotchColour(n = n),
+                 ...)
+}
+
--- R/scotchColours.R
+++ R/scotchColours.R
@@ -1,24 +1,19 @@
 #' @param n The number of colours needed from Set 1 from library(RColorBrewer)
 #' @title Better colour palette than Set 1 from library(RColorBrewer)
+#' @return Returns a function of colours
 #' @examples
-#' df = data.frame(
-#' x = rnorm(100),
-#' y = rnorm(100),
-#' colour = factor(1:10))
-#' ggplot(df, aes(x = x, y = y, colour = colour)) +
-#' geom_point() +
-#' scale_colour_manual(values = scotchColour(n = df$colour %>% unique %>% length))
-#'
-scotchColour = function(n){
+#' show_col(scotchColour()(10))
+scotchColour = function(){
   set1Colours = RColorBrewer::brewer.pal(9, "Set1")
   set1Colours[6] = "#FFFF33"
   set1Colours[9] = "#666666"
 
-  if (n <= 9){
-    nColours = set1Colours[1:n]
-  } else {
-    nColours = grDevices::colorRampPalette(set1Colours)(n)
-  }
+  # if (n <= 9){
+  #   nColours = set1Colours[1:n]
+  # } else {
+  #   nColours = grDevices::colorRampPalette(set1Colours)(n)
+  # }
 
+  nColours = grDevices::colorRampPalette(set1Colours)
   return(nColours)
 }

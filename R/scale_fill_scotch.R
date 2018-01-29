ccc564827d30bf5bfdb0517c2a1afe22f541d7e4
kevinw <kevin.wang@sydney.edu.au>
2018-01-29 13:47:09 +1100

    scale_fill_scotch

---
+++ R/scale_fill_scotch.R
@@ -0,0 +1,20 @@
+#' @title Scotch colours.
+#' @param n The number of colours
+#' @return ggproto object
+#' @examples
+#' df = data.frame(
+#' x = rnorm(100),
+#' y = rnorm(100),
+#' colour = factor(1:10))
+#' p = ggplot(df, aes(x = x, y = y, colour = colour)) +
+#' geom_point()
+#' p + scale_colour_scotch()
+#'
+scale_fill_scotch = function (stata = FALSE, ...)
+{
+  discrete_scale(aesthetics = "fill",
+                 scale_name = "scotch",
+                 palette = SmokyScotch::scotchColour(stata = stata),
+                 ...)
+}
+

73a835cbc1a01e64bf5bb9b646ffc9fd4d80977c
kevinwang09 <kevin.wang@sydney.edu.au>
2018-04-11 16:34:06 +1000

    changed clean_names_char

--- R/clean_names_char.R
+++ R/clean_names_char.R
@@ -2,24 +2,26 @@
 #'
 #' @param x a character vector
 #' @import magrittr
+#' @import snakecase
 #' @export
 #' @return A cleaned character vector
 #'
 #'
 #'
 #'
-clean_names_char <- function(x){
+clean_names_char <- function(x, case = c("snake", "lower_camel", "upper_camel",
+                                         "screaming_snake", "lower_upper", "upper_lower", "all_caps",
+                                         "small_camel", "big_camel", "old_janitor", "parsed", "mixed")){
   old_names <- x
   new_names <- old_names %>% gsub("'", "", .) %>% gsub("\"",
-                                                       "", .) %>% gsub("%", "percent", .) %>% gsub("^[ ]+",
-                                                                                                   "", .) %>% make.names(.) %>% gsub("[.]+", "_", .) %>%
-    gsub("[_]+", "_", .) %>% tolower(.) %>% gsub("_$", "",
-                                                 .)
-  dupe_count <- sapply(1:length(new_names), function(i) {
+                                                       "", .) %>% gsub("%", ".percent_", .) %>% gsub("#", ".number_",
+                                                                                                     .) %>% gsub("^[[:space:][:punct:]]+", "", .) %>% make.names(.) %>%
+    snakecase::to_any_case(case = case, sep_in = "\\.", transliterations = c("Latin-ASCII"),
+                           parsing_option = 4)
+  dupe_count <- vapply(seq_along(new_names), function(i) {
     sum(new_names[i] == new_names[1:i])
-  })
+  }, integer(1))
   new_names[dupe_count > 1] <- paste(new_names[dupe_count >
                                                  1], dupe_count[dupe_count > 1], sep = "_")
-
   return(new_names)
 }

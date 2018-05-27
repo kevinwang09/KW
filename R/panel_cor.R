e07aa99d62d5ddaf7e4a8f4fdf570f70a638d981
kevinwang09 <kevin.wang@sydney.edu.au>
2018-05-27 11:03:08 +1000

    more pairs panel functions

--- R/panel_cor.R
+++ R/panel_cor.R
@@ -1,12 +1,11 @@
 #' A better pairs plot function
 #'
 #' @param data a data frame
-#' @param subset A subsetting index
 #' @export
 #' @examples
-#' pairs_cor(iris[,-5])
+#' pairs(iris[,-5], lower.panel = panel_cor)
 
-panel.cor <- function(x, y)
+panel_cor <- function(x, y)
 {
   usr <- par("usr"); on.exit(par(usr))
   par(usr = c(0, 1, 0, 1))
@@ -19,19 +18,4 @@ panel.cor <- function(x, y)
   #   text(0.5, 0.25, paste("subset Corr=",txt), cex = 2)
   # }
 
-}
-
-panel.scatter <- function(x, y)
-{
-  # usr <- par("usr"); on.exit(par(usr))
-  # par(usr = c(0, 1, 0, 1))
-  # points(x, y, col = ifelse(subset, "black", "red"))
-  points(x, y)
-}
-
-
-pairs_cor = function(data){
-  pairs(x = data,
-        lower.panel = panel.cor,
-        upper.panel = panel.scatter)
 }
---
+++ R/panel_scatter_abline.R
@@ -0,0 +1,28 @@
+#' A better pairs plot function
+#'
+#' @param data a data frame
+#' @export
+#' @examples
+#' pairs(iris[,-5],
+#' lower.panel = panel_cor,
+#' upper.panel = panel_scatter_abline)
+
+panel_scatter_abline <- function(x, y, a = 0, b = 1, col = "red")
+{
+  points(x, y)
+  abline(a = a, b = b, col = col)
+}
+
+
+panel_scatter_vline <- function(x, y, v = 0, col = "red")
+{
+  points(x, y)
+  abline(v = v, col = col)
+}
+
+
+panel_scatter_hline <- function(x, y, h = 0, col = "red")
+{
+  points(x, y)
+  abline(h = h, col = col)
+}

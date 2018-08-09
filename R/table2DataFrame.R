9bc6780a6a7a11db7e42cb684688bdd27935da7b
kevinwang09 <kevin.wang@sydney.edu.au>
2018-08-09 10:17:25 +1000

    table2DataFrame

---
+++ R/table2DataFrame.R
@@ -0,0 +1,13 @@
+#' Turn a table into a data frame
+#' @param x a frequency table (i.e. only one row)
+#' @param variable.name a character 
+#' @param value.name a character
+table2DataFrame = function(x, variable.name = "variable.name", value.name = "value.name"){
+  res = data.frame(
+    names(x),
+    as.numeric(x)
+  )
+  
+  names(res) = c(variable.name, value.name)
+  return(res)
+}

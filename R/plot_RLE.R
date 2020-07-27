88db41867908f1cd29397d8e2925b260e2095a1e
Kevin Wang <kevin.wang@sydney.edu.au>
2020-07-27 23:11:55 +1000

    fix plot_RLE

--- R/plot_RLE.R
+++ R/plot_RLE.R
@@ -1,6 +1,5 @@
 #' RLE plot
 #' @param exprsMatrix a matrix
-#' @param colour colours corresponding to samples
 #' @author Kevin Wang
 #' @import ggplot2
 #' @export
@@ -13,17 +12,10 @@
 #' )
 #' boxplot(exprsMatrix)
 #' plot_RLE(exprsMatrix)
-#' plot_RLE(exprsMatrix, colour = sample(1:5, n, replace = TRUE))
-
-
-plot_RLE = function(exprsMatrix, colour = NULL){
+plot_RLE = function(exprsMatrix){
 
   if(is.null(colnames(exprsMatrix))){
     colnames(exprsMatrix) = paste0("sample_", 1:ncol(exprsMatrix))
-  }
-
-  if(is.null(colour)){
-    colour = rep("black", ncol(exprsMatrix))
   }
 
   sampleMedian = apply(exprsMatrix, 2, median)
@@ -34,33 +26,20 @@ plot_RLE = function(exprsMatrix, colour = NULL){
     sampleID = colnames(exprsMatrix) %>% forcats::as_factor(),
     sampleMedian = sampleMedian,
     sampleQ1 = sampleQ1,
-    sampleQ3 = sampleQ3,
-    colour = as.character(colour)
+    sampleQ3 = sampleQ3
   )
 
-  if(unique(colour) == "black"){
-    rlePlot = rlePlotdf %>%
-      ggplot2::ggplot(aes(x = sampleID,
-                          y = sampleMedian,
-                          label = sampleID)) +
-      geom_point() +
-      geom_errorbar(aes(ymin = sampleQ1, ymax = sampleQ3), width = 0) +
-      geom_hline(yintercept = 0, colour = "red") +
-      theme_classic(18) +
-      theme(legend.position = "bottom",
-            axis.text.x = element_text(angle = 90))
-  } else {
-      rlePlot = rlePlotdf %>%
-        ggplot2::ggplot(aes(x = sampleID,
-                            y = sampleMedian,
-                            label = sampleID, colour = colour)) +
-        geom_point() +
-        geom_errorbar(aes(ymin = sampleQ1, ymax = sampleQ3), width = 0) +
-        geom_hline(yintercept = 0, colour = "red") +
-        theme_classic(18) +
-        theme(legend.position = "bottom",
-              axis.text.x = element_text(angle = 90))
-  }
+
+  rlePlot = rlePlotdf %>%
+    ggplot2::ggplot(aes(x = sampleID,
+                        y = sampleMedian,
+                        label = sampleID)) +
+    geom_point() +
+    geom_errorbar(aes(ymin = sampleQ1, ymax = sampleQ3), width = 0) +
+    geom_hline(yintercept = 0, colour = "red") +
+    theme_classic(18) +
+    theme(legend.position = "bottom",
+          axis.text.x = element_text(angle = 90))
 
 
   result = list(
